c43455b94655af87faed570e3271f460aa08bee1
Jan Oldřich Krůza <jan.kruza@gooddata.com>
2020-08-29 15:22:28 +0200

    zoom map

--- src/scenes/Main_scene.ts
+++ src/scenes/Main_scene.ts
@@ -17,23 +17,28 @@ export class Main_scene extends Phaser.Scene {
     super('demo');
   }
 
+  cam() {
+    return this.cameras.main;
+  }
+
   preload() {
     this.load.image('map', 'assets/map.jpg');
   }
 
   create() {
-    const camera = this.cameras.main;
-    camera.setBounds(0, 0, map_width, map_height);
-    camera.setScroll(map_center.x - viewport_width / 2, map_center.y - viewport_height / 2);
+    this.cam().setBounds(0, 0, map_width, map_height);
+    this.cam().setScroll(map_center.x - viewport_width / 2, map_center.y - viewport_height / 2);
+    this.cam().setBackgroundColor('#FFFFFF');
 
     const map = this.add.image(0, 0, 'map').setOrigin(0);
 
     this.input.on('pointerdown', (pointer) => {
       this.panning = true;
-      this.panning_origin_x = pointer.downX + camera.scrollX;
-      this.panning_origin_y = pointer.downY + camera.scrollY;
+      this.panning_origin_x = pointer.downX + this.cam().scrollX;
+      this.panning_origin_y = pointer.downY + this.cam().scrollY;
     });
     this.input.on('pointermove', (evt) => this.handle_mouse_move(evt));
+    this.input.on('wheel', (pointer, objs, dx, dy, dz) => this.handle_mousewheel(pointer, objs, dx, dy, dz));
   }
 
   handle_mouse_move(evt) {
@@ -49,8 +54,21 @@ export class Main_scene extends Phaser.Scene {
     const pos = evt.position;
     const dx = this.panning_origin_x - pos.x;
     const dy = this.panning_origin_y - pos.y;
-    this.cameras.main.scrollX = dx;
-    this.cameras.main.scrollY = dy;
+    this.cam().scrollX = dx;
+    this.cam().scrollY = dy;
+  }
+
+  handle_mousewheel(pointer, objs, dx, dy, dz) {
+    this.zoom(dy);
+  }
+
+  zoom(delta) {
+    const new_zoom = this.cam().zoom * (1 - delta / 10);
+    const min_h = viewport_height / map_height;
+    const min_w = viewport_width / map_width;
+    const min_zoom = Math.max(new_zoom, Math.min(min_h, min_w));
+    const max_zoom = 1;
+    const clamped_new_zoom = Math.min(Math.max(new_zoom, min_zoom), max_zoom);
+    this.cam().setZoom(clamped_new_zoom);
   }
 }
-
