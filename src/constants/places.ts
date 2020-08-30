48af447a5ba4cc06280ef05aeaba161017ce9070
Jan Oldřich Krůza <jan.kruza@gooddata.com>
2020-08-30 21:07:54 +0200

    more map import

---
+++ src/constants/places.ts
@@ -0,0 +1,71 @@
+export const gate_1 = 'gate_1';
+export const gate_2 = 'gate_2';
+export const gate_3 = 'gate_3';
+export const gate_4 = 'gate_4';
+export const gate_5 = 'gate_5';
+export const gate_6 = 'gate_6';
+export const gate_7 = 'gate_7';
+
+export const gates = [
+  gate_1,
+  gate_2,
+  gate_3,
+  gate_4,
+  gate_5,
+  gate_6,
+  gate_7,
+];
+
+export const crossroard_1  = 'crossroad_1';
+export const crossroard_2  = 'crossroad_2';
+export const crossroard_3  = 'crossroad_3';
+export const crossroard_4  = 'crossroad_4';
+export const crossroard_5  = 'crossroad_5';
+export const crossroard_6  = 'crossroad_6';
+export const crossroard_7  = 'crossroad_7';
+export const crossroard_8  = 'crossroad_8';
+export const crossroard_9  = 'crossroad_9';
+export const crossroard_10 = 'crossroad_10';
+export const crossroard_11 = 'crossroad_11';
+export const crossroard_12 = 'crossroad_12';
+
+export const crossroads = [
+  crossroard_1,
+  crossroard_2,
+  crossroard_3,
+  crossroard_4,
+  crossroard_5,
+  crossroard_6,
+  crossroard_7,
+  crossroard_8,
+  crossroard_9,
+  crossroard_10,
+  crossroard_11,
+  crossroard_12,
+];
+
+export const teleport_1 = 'teleport_1';
+export const teleport_2 = 'teleport_2';
+export const teleport_3 = 'teleport_3';
+export const teleport_4 = 'teleport_4';
+export const teleport_5 = 'teleport_5';
+export const teleport_6 = 'teleport_6';
+
+export const teleports = [
+  teleport_1,
+  teleport_2,
+  teleport_3,
+  teleport_4,
+  teleport_5,
+  teleport_6,
+];
+
+export const white_crossroad_7 = 'white_crossroad_7';
+export const white_crossroad_12 = 'white_crossroad_12';
+
+export const white_crossroads = [
+  null, null, null, null, null, null,
+  white_crossroad_7,
+  null, null, null, null,
+  white_crossroad_12,
+];
