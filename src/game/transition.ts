57b7c1f4f3e14a7da2f0210c0b6309e7fa3595c0
Jan Oldřich Krůza <jan.kruza@gooddata.com>
2024-01-20 17:17:20 +0100

    restrict path of grace transitions

--- src/game/transition.ts
+++ src/game/transition.ts
@@ -18,8 +18,30 @@ export function transitions(current_place: Places.Place_name, previous_place: Pl
     const adjacent_roads = get_adjacent_roads(current_place).map(x => x.road);
     let possible_next_roads = adjacent_roads.filter(r => r !== previous_place);
     if (current_place === Places.gate_6) {
-      // white_crossroad_12_to_gate_6 is one-way
-      possible_next_roads = possible_next_roads.filter(r => r !== Places.white_crossroad_12_to_gate_6);
+      if (previous_place === Places.gate_6_to_crossroad_9 || previous_place === Places.world_center) {
+        // cannot enter the path of grace neither when dying through gate 6 nor when entering from gate 5
+        possible_next_roads = possible_next_roads.filter(r => r !== Places.white_crossroad_12_to_gate_6);
+      }
+      if (previous_place === Places.white_crossroad_12_to_gate_6) {
+        // path of grace ends 
+        possible_next_roads = possible_next_roads.filter(r => r !== Places.gate_6_to_crossroad_9);
+      }
+    }
+    if (current_place === Places.gate_7 && previous_place === Places.gate_7_to_white_crossroad_12) {
+      // turn around when hitting gate 7 from the path of grace
+      possible_next_roads = [Places.gate_7_to_white_crossroad_12];
+    }
+    if (current_place === Places.white_crossroad_12 && previous_place === Places.gate_7_to_white_crossroad_12) {
+      // only can go straight when passing white crossroad 12 from gate 7
+      possible_next_roads = [Places.white_crossroad_12_to_gate_6];
+    }
+    if (current_place === Places.gate_7 && previous_place === Places.gate_7_to_crossroad_3) {
+      // cannot enter the path of grace when coming to gate 7 from the world
+      possible_next_roads = [];
+    }
+    if (current_place === Places.crossroad_8) {
+      // cannot enter the path of grace from crossroad 8
+      possible_next_roads = possible_next_roads.filter(r => r !== Places.white_crossroad_7_to_crossroad_8);
     }
     return possible_next_roads;
     // return adjacent_roads.length > 1 ? adjacent_roads : possible_next_roads; // DEBUG: Allow turn-around
