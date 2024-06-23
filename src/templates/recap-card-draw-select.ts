5733129dcc48080e824f70dccef8121cf4d14796
Jan Oldřich Krůza <jan.kruza@gooddata.com>
2024-06-23 17:44:42 +0200

    better wording in card select recap

--- src/templates/recap-card-draw-select.ts
+++ src/templates/recap-card-draw-select.ts
@@ -23,25 +23,45 @@ interface Card_draw_select_props {
   draw_event: Select_from_presented_cards;
 }
 
-const get_context = (draw_event: Select_from_presented_cards) => {
+interface Wording {
+  from_these_cards: string;
+  you_picked: string;
+  by_which: string;
+}
+const default_wording: Wording = {
+  from_these_cards: 'Z těchto karet:',
+  you_picked: 'sis vybral tuto.',
+  by_which: '',
+};
+
+const get_wording = (draw_event: Select_from_presented_cards): Wording => {
   const next_event = find_event_forward(is_Game_event, draw_event);
   if (!next_event) {
-    return [null, null];
+    return default_wording;
   }
   const drawing_place = get_current_place(draw_event);
   if (drawing_place === world_center && is_Birth_gate_select(next_event)) {
     const gate_id = next_event.payload.place_name;
     const gate_name = birth_gate_names[gate_id];
-    return ['Při volbě brány odhodlání,', `Tím's vstoupil do brány „${gate_name}“.`];
+    const gate_number = gate_id.match(/\d+/)![0];
+    return {
+      from_these_cards: 'Při volbě brány zrození sis z těchto karet:',
+      you_picked: 'vybral tuto:',
+      by_which: `Tím's vstoupil do brány č. ${gate_number} „${gate_name}“.`,
+    };
   }
   if (is_crossroad(drawing_place) && is_Enter_road(next_event)) {
     const starting_crossroad_lang = place_language_expression(drawing_place, Grammatical_case.dative);
     const road_name = next_event.payload.place_name;
     const road_connection = road_connects(road_name, drawing_place);
     const other_road_end_lang = place_language_expression(road_connection.destination, Grammatical_case.dative);
-    return [`Na ${starting_crossroad_lang}`, `Tím ses vydal směrem ke ${other_road_end_lang}.`];
+    return {
+      from_these_cards: `Na ${starting_crossroad_lang} sis z těchto karet:`,
+      you_picked: 'vybral tuto:',
+      by_which: `Tím ses vydal směrem ke ${other_road_end_lang}.`,
+    };
   }
-  return [null, null];
+  return default_wording;
 };
 
 const html = htm.bind(h);
@@ -52,10 +72,14 @@ export class Card_draw_select extends Component<Card_draw_select_props> {
     const { id: picked, set } = draw_event.payload.card;
     const picked_meta = card_meta[picked];
     const offer = offer_event.payload.cards;
-    const [context_pre, context_post] = get_context(draw_event);
+    const {
+      from_these_cards,
+      you_picked,
+      by_which,
+    } = get_wording(draw_event);
     return html`
       <div class="recap-card-offer">
-        <p>${context_pre} z těchto karet:</p>
+        <p>${from_these_cards}</p>
         <ul>
           ${offer.map((card: Card) => html`
             <li key=${card}><img src="assets/cards/${set}/${card}.jpg" alt="" />${card_meta[card].name_cs}</li>
@@ -63,10 +87,10 @@ export class Card_draw_select extends Component<Card_draw_select_props> {
         </ul>
       </div>
       <div class="recap-picked-card">
-        <p>sis vybral tuto:</p>
+        <p>${you_picked}</p>
         <img src="assets/cards/${set}/${picked}.jpg" alt="" />${picked_meta.name_cs}
         <p class="recap-exegesis">${picked_meta.exegesis}.</p>
-        ${context_post && html`<p>${context_post}</p>`}
+        ${by_which}
       </div>
     `;
   }
