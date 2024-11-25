ab5b8b361afdc116440fd70fcfab27d256002f85
Jan Oldřich Krůza <jan@sixtease.net>
2024-11-25 17:27:22 +0100

    use exit gate names for death

--- src/guide/exiting-world.ts
+++ src/guide/exiting-world.ts
@@ -2,14 +2,14 @@ import { h, Component, render } from 'preact';
 import htm from 'htm';
 
 import { get_current_place } from '../game/logic';
-import { birth_gate_names } from '../constants/guide';
+import { death_gate_names } from '../constants/guide';
 
 const html = htm.bind(h);
 
 export class Exiting_world extends Component<{}> {
   render() {
     const exit_gate = get_current_place();
-    const exit_gate_name = birth_gate_names[exit_gate];
+    const exit_gate_name = death_gate_names[exit_gate];
 
     return html`
       <div class="guide-page guide-page--exiting-world">
--- src/guide/game-over.ts
+++ src/guide/game-over.ts
@@ -2,14 +2,14 @@ import { h, Component, render } from 'preact';
 import htm from 'htm';
 
 import { get_previous_place } from '../game/logic';
-import { birth_gate_names } from '../constants/guide';
+import { death_gate_names } from '../constants/guide';
 
 const html = htm.bind(h);
 
 export class Game_over extends Component<{}> {
   render() {
     const exit_gate = get_previous_place();
-    const exit_gate_name = birth_gate_names[exit_gate];
+    const exit_gate_name = death_gate_names[exit_gate];
 
     return html`
       <div class="guide-page guide-page--exiting-world">
